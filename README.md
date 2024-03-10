e813455d914af81a047b4791956ec9beadd4e86e
Natalia DaLomba <natalia@starforce.games>
2024-03-10 17:39:51 -0500

    Added runtime system support (add/remove)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Service Locator (Unity Package)
 This is an implementation of the service locator pattern in Unity using MonoBehaviours, and a prefab in Resources.
+
+Because this repo is in a Unity package format, you must include these files directly as an Embedded Unity Package into your own Unity project to work on this repo. It's recommended to use a Git submodule to let you make commits back to this repo directly.
--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -2,37 +2,58 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Represents an object that has a list of all major game systems in the running app (in playmode only).
-/// </summary>
-[DefaultExecutionOrder(-10000)]
-public class ServiceLocator : MonoBehaviour {
-    private static ServiceLocator instance;
-    public static ServiceLocator Instance {
-        get {
-            if (instance == null) {
-                ServiceLocator prefab = Resources.Load<ServiceLocator>("Service Locator");
-                Component.Instantiate(prefab);
+namespace SFG.ServiceLocator {
+    /// <summary>
+    /// Represents an object that has a list of all major game systems in the running app (in playmode only).
+    /// </summary>
+    [DefaultExecutionOrder(-10000)]
+    public class ServiceLocator : MonoBehaviour {
+        private static ServiceLocator instance;
+        public static ServiceLocator Instance {
+            get {
+                if (instance == null) {
+                    ServiceLocator prefab = Resources.Load<ServiceLocator>("Service Locator");
+                    Component.Instantiate(prefab);
+                }
+                return instance;
             }
-            return instance;
         }
-    }
 
-    private void Awake() {
-        if (instance != null && instance != this) {
-            GameObject.DestroyImmediate(gameObject);
-            Debug.LogError("Duplicate " + nameof(ServiceLocator) + " was found and automatically deleted!");
-            return;
+        private List<MonoBehaviour> runtimeSystems = new();
+
+        private void Awake() {
+            if (instance != null && instance != this) {
+                GameObject.DestroyImmediate(gameObject);
+                Debug.LogError("Duplicate " + nameof(ServiceLocator) + " was found and automatically deleted!");
+                return;
+            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
 
-    public T GetSystem<T>() {
-        T service = GetComponentInChildren<T>();
+        public T GetSystem<T>() where T : class {
+            T system = null;
+            foreach (MonoBehaviour other in runtimeSystems)
+                if (other is T specificType)
+                    system = specificType;
 
-        if (service == null) 
-            Debug.LogWarning("Service of type " + typeof(T).Name + " not found.");
-        return service;
+            if (system == null)
+                system = GetComponentInChildren<T>();
+
+            if (system == null)
+                Debug.LogWarning("Service of type " + typeof(T).Name + " not found.");
+            return system;
+        }
+
+        public bool AddRuntimeSystem<T>(T system) where T : MonoBehaviour {
+            if (runtimeSystems.Contains(system))
+                return false;
+            runtimeSystems.Add(system);
+            return true;
+        }
+
+        public bool RemoveRuntimeSystem<T>(T system) where T : MonoBehaviour {
+            return runtimeSystems.Remove(system);
+        }
     }
 }
